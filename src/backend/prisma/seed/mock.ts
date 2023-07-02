0665d890ca5509ebe9151194247b40ca600ee442
yahav <yahav2011@gmail.com>
2023-07-02 08:58:53 +0300

    added validations to db schemas + change relations

--- src/backend/prisma/seed/mock.ts
+++ src/backend/prisma/seed/mock.ts
@@ -2,7 +2,7 @@ const mock = [
   {
     name: 'projects-manager',
     description: 'A service that manage other projects',
-    url: 'https//:projects-manager.com',
+    url: 'https://projects-manager.com',
     maintenance: {
       create: {
         hours: 3,
@@ -12,7 +12,7 @@ const mock = [
   {
     name: 'backlog',
     description: 'A service that store and prioritize features and bugs',
-    url: 'https//:backlog.com',
+    url: 'https://backlog.com',
     maintenance: {
       create: {
         hours: 3,
@@ -22,7 +22,7 @@ const mock = [
   {
     name: 'portfolio',
     description: 'A frontend service that display a portfolio',
-    url: 'https//:portfolio.com',
+    url: 'https://portfolio.com',
     maintenance: {
       create: {
         hours: 3,
