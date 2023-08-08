0e29580d6e84f9a57acb682281c29900457b3826
Sasporta <69085015+Sasporta@users.noreply.github.com>
2023-08-08 23:12:31 +0300

    adding docker volumes (#21)

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,9 +2,6 @@
 npm run lint
 npx tsc
 docker-compose up -d
-# TODO: replace with wait-for-it.sh or move db init to docker-compose
-sleep 2
-npx prisma migrate dev
 jest --maxWorkers=1 --collectCoverage --silent $1
 docker-compose down
 docker volume prune -f
